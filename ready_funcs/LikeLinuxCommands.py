826618c3bcde95c08c42c1ddca39652bc5f01009
nekoreal <amakoev8938@gmail.com>
2026-02-20 13:18:48 +0300

    Исправление миниошибки

--- ready_funcs/LikeLinuxCommands.py
+++ ready_funcs/LikeLinuxCommands.py
@@ -11,7 +11,7 @@ def cli():
 @cli.command()
 @click.option('-u', '--user_id', type=int, help='ID пользователя')
 @click.option('-r', '--recursive', is_flag=True, help='Рекурсивно')
-@click.argument('category', help="123")
+@click.argument('category')
 def stats(user_id, recursive, category):
     print(f"Выполняю STATS: пользователь={user_id}, рекурсия={recursive}, категория={category}")
 
